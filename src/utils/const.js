9695b6bbb9fe9b2253967b16d10d168a3f8cf9a5
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2022-12-06 09:56:00 +0200

    add serv url

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,4 @@
 export const ADD_DATA_FROM_SERVER = 'ticker';
-export const SERVER_URL = 'https://guileless-beijinho-0bb0d4.netlify.app';
+export const SERVER_URL = 'https://socket-server-tawny.vercel.app';
 export const SET_ON_OFF_STATUS = 'SET_ON_OFF_STATUS';
 export const REMOVE_INDEX = 'REMOVE_INDEX';
